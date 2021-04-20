9e668a884d864a9695e7712ca8cfeb35f4ca68b3
krisharekar <74191269+krisharekar@users.noreply.github.com>
2021-04-20 11:35:51 +0530

    daily donos

--- donation-trackers/daily-donations.js
+++ donation-trackers/daily-donations.js
@@ -11,6 +11,8 @@ module.exports = async (client) => {
 async function resetDailyDonations (client) {
     client.guilds.cache.forEach(async g => {
         const guildId = g.id
-        await donationsSchema.updateMany({ guildId, dailyDonation: { $ne: 0 } }, { dailyDonation: 0 }, { new: true })    
+        await donationsSchema.updateMany({ guildId, dailyDonation: { $ne: 0 } }, { dailyDonation: 0 }, { new: true }) 
+        const channel = client.channels.cache.get('775970225279074327')
+        channel.send(`UPDATED ${g.name}'s daily donations.`)
     })
 }
