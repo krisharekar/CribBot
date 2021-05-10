a3db113f9c5c8a9c50ed9a71300220c53f153b0d
krisharekar <74191269+krisharekar@users.noreply.github.com>
2021-05-10 20:07:05 +0530

    ffs

--- commands/misc/invite.js
+++ commands/misc/invite.js
@@ -8,7 +8,11 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setAuthor(`Bot's links`, client.user.displayAvatarURL())
         .setColor('BLUE')
-        .setDescription(`[Bot's Invite Link](https://discord.com/oauth2/authorize?client_id=744210245403934780&permissions=4294967287&scope=bot)\n[Bot's Invite Link (admin)](https://discord.com/api/oauth2/authorize?client_id=744210245403934780&permissions=8&scope=bot)`)
+        .setDescription([`[Bot's Invite Link](https://discord.com/oauth2/authorize?client_id=744210245403934780&permissions=4294967287&scope=bot)`,
+        `[Bot's Invite Link (admin)](https://discord.com/api/oauth2/authorize?client_id=744210245403934780&permissions=8&scope=bot)`,
+        `[Bot's Support Server](https://discord.gg/cpWeW7apHD)`,
+        `[Vote the Bot on Top.gg](https://top.gg/bot/744210245403934780/vote)`,
+        `[Vote the Bot on Discord Bot List](https://discordbotlist.com/bots/crib-bot/upvote)`])
 
         message.channel.send(embed)
     }
--- commands/owner-only/find-freeloaders.js
+++ commands/owner-only/find-freeloaders.js
@@ -15,11 +15,11 @@ module.exports = {
         if (!channel)
             return message.channel.send('Specify a channel that exists.')
 
-        if(isNaN(limit))
-        return message.channel.send('Message count must be an integer')
+        if (isNaN(limit))
+            return message.channel.send('Message count must be an integer')
 
-        if(limit < 1 || limit > 1000)
-        return message.channel.send('Message count must be between 1-1000.')
+        if (limit < 1 || limit > 1000)
+            return message.channel.send('Message count must be between 1-1000.')
 
         message.channel.send('Searching for stinky freeloaders...')
 
@@ -29,22 +29,24 @@ module.exports = {
 
         for (const msg of messages) {
             const exists = message.guild.members.cache.get(msg.author.id)
-            if (!exists && msg.author.bot)
-            return;
-            if (!exists) {
-                const bans = await message.guild.fetchBans()
-                if(bans.find(u => u.user == msg.author) && !bannedFreeloaders.find(key => key == `${msg.author.username} (${msg.author.id})`))
-                bannedFreeloaders.push(`${msg.author.username} (${msg.author.id})`)
-                // bannedFreeloaders.push({ name: msg.author.username, id: msg.author.id })
+            if (!exists && msg.author.bot) {
+                if (!exists) {
+                    const bans = await message.guild.fetchBans()
+                    if (bans.find(u => u.user == msg.author)) {
+                        if (!bannedFreeloaders.find(key => key == `${msg.author.username} (${msg.author.id})`))
+                            bannedFreeloaders.push(`${msg.author.username} (${msg.author.id})`)
+                        // bannedFreeloaders.push({ name: msg.author.username, id: msg.author.id })
+                    }
 
-                else if(!freeloaders.find(key => key == `${msg.author.username} (${msg.author.id})`))
-                freeloaders.push(`${msg.author.username} (${msg.author.id})`)
-                // freeloaders.push({ name: msg.author.username, id: msg.author.id })
+                    else if (!freeloaders.find(key => key == `${msg.author.username} (${msg.author.id})`))
+                        freeloaders.push(`${msg.author.username} (${msg.author.id})`)
+                    // freeloaders.push({ name: msg.author.username, id: msg.author.id })
+                }
             }
         }
 
-        if(!freeloaders.length && !bannedFreeloaders.length)
-        return message.channel.send('No freeloaders found!')
+        if (!freeloaders.length && !bannedFreeloaders.length)
+            return message.channel.send('No freeloaders found!')
 
         // console.log(messages.length)
 
@@ -61,11 +63,18 @@ module.exports = {
         //     bannedFreeloaderMessage += `${bannedFreeloader.name} (${bannedFreeloader.id})\n`
         // }
 
-        message.channel.send(`FREELOADER ALERT!\n\nFreeloaders are:\n${freeloaders.length ? freeloaders.join('\n') : 'None'}\n\nFreeloaders who are already banned are:\n${bannedFreeloaders.length ? bannedFreeloaders.join('\n') : 'None'}`)
+        const content = `FREELOADER ALERT!\n\nFreeloaders are:\n${freeloaders.length ? freeloaders.join('\n') : 'None'}\n\nFreeloaders who are already banned are:\n${bannedFreeloaders.length ? bannedFreeloaders.join('\n') : 'None'}`
+
+        if (content.length > 2000) {
+            // const c = content.match(/(.|[\r\n]){1,10}/g)
+            // console.log(c)
+            return message.channel.send('Damn, found too many freeloaders that Discord wont let me sent such a huge message :C\nMaybe try reducing message count.')
+        }
+        message.channel.send(content).then(() => console.log('sent'))
     }
 }
 
-async function fetchMessages(channel, limit = 500) {
+async function fetchMessages(channel, limit = 200) {
     const allMessages = [];
     let lastId;
 
@@ -77,9 +86,11 @@ async function fetchMessages(channel, limit = 500) {
 
         const messages = await channel.messages.fetch(options).catch(e => console.log(e))
         allMessages.push(...messages.array());
+        console.log(allMessages.length)
         lastId = messages.last().id;
 
         if (messages.size != 100 || allMessages.length >= limit) {
+            console.log('done')
             break;
         }
     }
