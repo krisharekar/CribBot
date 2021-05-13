afb5efc59b5461e63113fee4abe1fdfe5d65a077
krisharekar <74191269+krisharekar@users.noreply.github.com>
2021-05-13 20:34:47 +0530

    ffs

--- commands/owner-only/find-freeloaders.js
+++ commands/owner-only/find-freeloaders.js
@@ -18,16 +18,29 @@ module.exports = {
         if (isNaN(limit))
             return message.channel.send('Message count must be an integer')
 
-        if (limit < 1 || limit > 1000)
-            return message.channel.send('Message count must be between 1-1000.')
+        // if (limit < 1 || limit > 1000)
+        //     return message.channel.send('Message count must be between 1-1000.')
 
-        message.channel.send('Searching for stinky freeloaders...')
+        const botMsg = await message.channel.send([`Searching for stinky freeloaders...`,
+        `Indexing: ${'□'.repeat(10)} 0%`,
+        `Searching: ${'□'.repeat(10)} 0%`])
 
-        const messages = await fetchMessages(channel, limit)
+        const messages = await fetchMessages(channel, limit, botMsg)
         const freeloaders = []
         const bannedFreeloaders = []
+        const number = messages.length / 100
+        const increment = 10 / number
+        let num = 0
+        let loaded = 0
 
         for (const msg of messages) {
+            num++
+            if(num%100 == 0 || number < 1) {
+                loaded = loaded+increment
+                await botMsg.edit([`Searching for stinky freeloaders...`,
+                `Indexing: ${'■'.repeat(10)} 100%`,
+                `Searching: ${'■'.repeat(loaded.toFixed() <= 10 ? loaded.toFixed() : 10)}${'□'.repeat(10 - loaded.toFixed() >= 0 ? 10 - loaded.toFixed() : 0)} ${Math.round(loaded * 10) <= 100 ? Math.round(loaded * 10) : 100}%`])
+            }
             const exists = message.guild.members.cache.get(msg.author.id)
             if (!exists && !msg.author.bot) {
                 if (!exists) {
@@ -66,18 +79,34 @@ module.exports = {
         const content = `FREELOADER ALERT!\n\nFreeloaders are:\n${freeloaders.length ? freeloaders.join('\n') : 'None'}\n\nFreeloaders who are already banned are:\n${bannedFreeloaders.length ? bannedFreeloaders.join('\n') : 'None'}`
 
         if (content.length > 2000) {
-            // const c = content.match(/(.|[\r\n]){1,10}/g)
-            // console.log(c)
+            const index = content.lastIndexOf("\n", 2000)
+            // const temp = content.slice(0, 2000)
+            const index2 = content.lastIndexOf("\n")
+            console.log(index, index2)
+
+            const newContent = []
+            do {
+                for (const freeloader of freeloaders) {
+                    newContent[0] += `${freeloader}\n`
+
+                    if (newContent.length > 2000)
+                        newContent.replace(freeloader, '')
+                }
+            } while (freeloaders)
+
             return message.channel.send('Damn, found so many freeloaders that Discord wont let me sent such a huge message :C\nMaybe try reducing message count.')
         }
         message.channel.send(content)
     }
 }
 
-async function fetchMessages(channel, limit = 200) {
+async function fetchMessages(channel, limit = 200, message) {
     const allMessages = [];
     let lastId;
-
+    const number = (limit / 100) // 500 = 500/100 = 5 = 10/5 = 2 
+    console.log(number)
+    const increment = 10 / number
+    let loaded = increment
     while (true) {
         const options = limit > 100 ? { limit: 100 } : { limit };
         if (lastId) {
@@ -86,6 +115,13 @@ async function fetchMessages(channel, limit = 200) {
 
         const messages = await channel.messages.fetch(options).catch(e => console.log(e))
         allMessages.push(...messages.array());
+        // console.log(loaded)
+        const content = [`Searching for stinky freeloaders...`,
+        `Indexing: ${'■'.repeat(loaded.toFixed() <= 10 ? loaded.toFixed() : 10)}${'□'.repeat(10 - loaded.toFixed() >= 0 ? 10 - loaded.toFixed() : 0)} ${Math.round(loaded * 10) <= 100 ? Math.round(loaded * 10) : 100}%`,
+        `Searching: ${'□'.repeat(10)} 0%`]
+        await message.edit(content)
+        loaded = loaded + increment
+        // console.log(loaded)
         // console.log(allMessages.length)
         lastId = messages.last().id;
 
