b0c8157d92aa53450daebb4a52787864a9a94ce4
krisharekar <74191269+krisharekar@users.noreply.github.com>
2021-05-21 17:47:44 +0530

    topgg

--- topgg-webhook.js
+++ topgg-webhook.js
@@ -7,7 +7,10 @@ const webhookURL = 'SLxBSiWBub3SXx7sPZokgOm9mr1wxT7bT3NF3v8h4nPfb6wAXlsVLc0nltUk
 const webhookURL2 = 'https://discord.com/api/webhooks/840900091606597674/SLxBSiWBub3SXx7sPZokgOm9mr1wxT7bT3NF3v8h4nPfb6wAXlsVLc0nltUktLoAQCHY'
 const DBL = require('dbl-api')
 module.exports = (client) => {
+    if(client.user.id == '780815332834410506')
+    return console.log('crib bot testing')
     const ap = AutoPoster(topggToken, client)
+
     ap.on('posted', async () => {
         const message = await client.channels.cache.get('840958539459723274').messages.fetch('840959070356766751')
         // console.log(message)
@@ -24,35 +27,4 @@ module.exports = (client) => {
 
         message.edit('', embed)
     }) //every 30mins
-
-    // const app = express()
-
-    // const webhook = new Topgg.Webhook(webhookURL)
-
-    // app.post("/dblwebhook", webhook.listener((vote) => {
-    //     console.log(vote.user)
-    //     const user = client.users.cache.get(vote.user)
-    //     const embed = new Discord.MessageEmbed()
-    //         .setAuthor(`${user ? user.tag : vote.user} just voted the bot!`, user ? user.displayAvatarURL() : '')
-    //         .setDescription(`Thank you for voting ${client.user.username} on Top.gg\nYou can vote the bot [here](http://top.gg/bot/744210245403934780/vote) every 12 hours!`)
-
-    //     const channel = client.channels.cache.get('840892150879092756')
-    //     if (channel)
-    //         channel.send(embed)
-    // }))
-    // const webhook = new Topgg.Webhook(webhookURL2)
-
-    // app.post('/webhook', webhook.listener((vote) => {
-    //     console.log(vote.user)
-    //     const user = client.users.cache.get(vote.user)
-    //     const embed = new Discord.MessageEmbed()
-    //         .setAuthor(`${user ? user.tag : vote.user} just voted the bot!`, user ? user.displayAvatarURL() : '')
-    //         .setDescription(`Thank you for voting ${client.user.username} on Top.gg\nYou can vote the bot [here](http://top.gg/bot/744210245403934780/vote) every 12 hours!`)
-
-    //     const channel = client.channels.cache.get('840892150879092756')
-    //     if (channel)
-    //         channel.send(embed)
-    // }))
-
-    // app.listen(80)
 }
