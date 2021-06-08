abd87d3bbd24b0f3a7cf0033b649f4a552beefa1
krisharekar <74191269+krisharekar@users.noreply.github.com>
2021-06-08 18:47:47 +0530

    weekly donos test

---
+++ donation-trackers/weekly-donations.js
@@ -0,0 +1,20 @@
+const donationsSchema = require('../schemas/donations-schema')
+const cron = require('node-cron')
+
+module.exports = async (client) => {
+    cron.schedule('0 0 0 * * 1', async () => {
+        await resetDailyDonations(client)
+    }, { timezone: 'Etc/UTC' })
+
+}
+
+async function resetDailyDonations(client) {
+    // let count = 0
+    // let failed = 0
+    // client.guilds.cache.forEach(async g => {
+    //     const guildId = g.id
+    //     await donationsSchema.updateMany({ guildId, dailyDonation: { $ne: 0 } }, { dailyDonation: 0 }, { new: true })
+    // })
+    const channel = client.channels.cache.get('775970225279074327')
+    channel.send(`UPDATED **${client.guilds.cache.size}** server's weekly donations.`)
+}
