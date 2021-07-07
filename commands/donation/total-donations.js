e3c9b49bdc8b032a8c8519f9de39ce1074e0c52e
krisharekar <74191269+krisharekar@users.noreply.github.com>
2021-07-07 14:51:21 +0530

    td

---
+++ commands/donation/total-donations.js
@@ -0,0 +1,21 @@
+const donationsSchema = require("../../schemas/donations-schema")
+
+module.exports = {
+    commands: ['total-donations', 'totaldonations', 'totaldonos', 'td'],
+
+    async execute(message, args) {
+        const guildId = message.guild.id
+        const result = await donationsSchema.find({ guildId })
+
+        if (!result) return message.channel.send('Total Donations Of The Server: \`⏣ 0\`.')
+
+        let totalDonations = 0
+
+        for (const donations of result) {
+            if (isNaN(donations.donationAmount)) continue;
+            totalDonations += donations.donationAmount
+        }
+
+        message.channel.send(`Total Donations Of The Server: \`⏣ ${totalDonations.toLocaleString()}\`.`)
+    }
+}
