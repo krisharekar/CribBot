68cc0980bd224142ae6827de8ef81eeedda74240
krisharekar <74191269+krisharekar@users.noreply.github.com>
2021-07-08 19:32:27 +0530

    removedc

--- commands/config/remove-donation-channel.js
+++ commands/config/remove-donation-channel.js
@@ -15,23 +15,23 @@ module.exports = {
 
         const result = await donationsChannelSchema.findOne({ guildId })
         // console.log(result)
-        
-        if(result && result.donationsChannelIds && !result.donationsChannelIds.includes(donationsChannelId))
-        return message.channel.send(`<#${donationsChannelId}> is not a donation channel.`)
+
+        if (result && result.donationsChannelIds && !result.donationsChannelIds.includes(donationsChannelId))
+            return message.channel.send(`<#${donationsChannelId}> is not a donation channel.`)
 
         const data = await donationsChannelSchema.findOneAndUpdate({ guildId }, { $pull: { donationsChannelIds: donationsChannelId } }, { upsert: true, new: true })
         loadCache()
 
         let desc = ''
 
-        for(const chan of data.donationsChannelIds) {
+        for (const chan of data.donationsChannelIds) {
             desc += `<#${chan}>\n`
         }
 
         const embed = new Discord.MessageEmbed()
-        .setAuthor(`Donation channel removed`, client.user.displayAvatarURL())
-        .setColor('GREEN')
-        .addField('Donations Channels:', desc)
+            .setAuthor(`Donation channel removed`, client.user.displayAvatarURL())
+            .setColor('GREEN')
+            .addField('Donations Channels:', desc ? desc : 'None')
 
         message.channel.send(`<#${donationsChannelId}> is no longer a donation channel.`, embed)
     }
